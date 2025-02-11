b82303f6e21a9b05c0b16a5597888f2b336542ee
Reihan Agam <reihanagam7@gmail.com>
2025-02-11 17:10:51 +0700

    update

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ const express = require("express")
 
 const ppob = require("./routes/ppob")
 const payment = require("./routes/payment")
+const callback = require("./routes/callback")
 
 const Route = express.Router()
 
 Route
     .use("/api/v1/ppob", ppob)
+    .use("/api/v1/callback", callback)
     .use("/api/v1/payment", payment)
 
 module.exports = Route
